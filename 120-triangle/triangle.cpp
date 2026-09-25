465a862bf9c1d8347921c9958acab6dea67badbc
rs9729958804-a11y <rs9729958804@gmail.com>
2026-09-25 21:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (66.63%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        // Copy the last row
+        vector<int> dp = triangle[n - 1];
+
+        // Start from second-last row
+        for (int i = n - 2; i >= 0; i--) {
+
+            for (int j = 0; j <= i; j++) {
+
+                dp[j] = triangle[i][j]
+                      + min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
